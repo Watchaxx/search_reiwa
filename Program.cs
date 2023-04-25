5474efab844e085ac4adacc7f339267c912c2a9e
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2023-04-25 11:59:10 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int r = 0;
+        string S = ReadLine();
+
+        foreach( var i in Range( 0, S.Length - 1 ) ) {
+            if( S.Substring( i, 2 ) == "令和" ) {
+                r++;
+            }
+        }
+        WriteLine( r );
+        return;
+    }
+}
